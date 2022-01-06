193eb645c504f455f75a8165d5abaf603b3e55c0
Raúl Pavón <raul.pavon.toral@gmail.com>
2022-01-06 12:09:52 -0600

    initial commit

--- Zeros/main.swift
+++ Zeros/main.swift
@@ -7,5 +7,13 @@
 
 import Foundation
 
-print("Hello, World!")
+var array = [5,0,4,0,8,1]
+var newArray: [Int] = []
+var zeroArray: [Int] = []
 
+for i in 0...array.count - 1 {
+    array[i] == 0 ? zeroArray.append(array[i]) : newArray.append(array[i])
+}
+
+newArray.append(contentsOf: zeroArray)
+print(newArray)
